2e2b89dead6a94176fe699b9868c3c60c80ead0d
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-07-11 20:25:37 +0100

    Add src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""Package d'initialisation pour le module src.gloire_dev_ia."""
+
+from .gloire_dev_ia import GloireDevIA
+from .security import SecurityAudit
+
+__all__ = ["GloireDevIA", "SecurityAudit"]
